35a3abacfa3681f8ef6a57043ac541f1d889682e
Akash Tiwari <akastiwa@cisco.com>
2024-06-19 22:05:38 +0530

    remove hard coded variables and error handling for views

--- NewsLetters/models.py
+++ NewsLetters/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from NewsLetters.consts import JSON_EMAIL_KEY, JSON_TOPIC_NAME_KEY
+
 
 # Possible Topics of the NewsLetter
 class Topic(models.Model):
@@ -11,13 +13,19 @@ class Topic(models.Model):
 
 # Subscriber Details identified by a unique email
 class Subscriber(models.Model):
-    email = models.EmailField(unique=True)
-    # # topic:Subscriber = 1:n
+    email = models.EmailField()
+    topic_name = models.CharField(max_length=100, blank=True)  # New field to store topic name
+    # topic:Subscriber = 1:n
     # Cascading deletion of subscribers in case of deletion of a topic
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
 
+    class Meta:
+        # Ensures that each (email, topic) pair is unique
+        unique_together = [JSON_EMAIL_KEY, JSON_TOPIC_NAME_KEY]
+
+    # email, topic_name should be unique
     def __str__(self):
-        return self.email
+        return self.email + " " + self.topic.name
 
 
 # Content of a SINGULAR newsLetter associated with a topic

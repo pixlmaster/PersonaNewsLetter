0c9e860a23daf3f47d6405d39bea8acf19fce058
Akash Tiwari <akastiwa@cisco.com>
2024-06-19 18:25:58 +0530

    Add View for Add subscriber and Add Content

--- NewsLetters/views.py
+++ NewsLetters/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
 from .models import Subscriber, Content, Topic
 import json
 

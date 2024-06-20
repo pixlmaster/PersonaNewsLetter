4d7fa9cd0953fcbf81e008024cafcb3bda17ba4e
Akash Tiwari <akastiwa@cisco.com>
2024-06-20 14:40:02 +0530

    increased thread pool

--- NewsLetters/management/commands/send_emails.py
+++ NewsLetters/management/commands/send_emails.py
@@ -1,18 +1,17 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
-
 import cachetools
 from django.core.management.base import BaseCommand
-
 from NewsLetters.consts import SENDER_EMAIL, STR_EMAIL_SENT_SUCCESSFULLY, MAX_WORKERS_SEND_EMAIL
 from NewsLetters.models import Content, Subscriber
 from django.core.mail import send_mail
 from django.utils import timezone
 import logging
 
-
+# Configure logging
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
-
+# Define the LRU cache
 subscriber_cache = cachetools.LRUCache(maxsize=10000)
 
 
@@ -26,9 +25,8 @@ def send_newsletters(content, subscribers):
                 [subscriber.email],
                 fail_silently=True
             )
-            print(STR_EMAIL_SENT_SUCCESSFULLY.format(subscriber.email))
+            logger.info(STR_EMAIL_SENT_SUCCESSFULLY.format(subscriber.email))
         except Exception as e:
-            print("Sending mail failed")
             logger.error(f"Failed to send email to {subscriber.email}: {str(e)}")
 
 
@@ -39,8 +37,6 @@ class Command(BaseCommand):
     Retrieves newsletter content scheduled for sending, iterates
     through each content to send emails to subscribers of its topic,
     and deletes the sent content after sending the emails.
-    Since the content for each subscriber list is the same, we can easily implment
-    multithreading to process each list of subscribers separately
     """
 
     help = 'Send scheduled newsletter emails'
@@ -48,35 +44,29 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         now = timezone.now()
         contents = Content.objects.filter(send_time__lte=now)
-        # clear the cache, it's going to serve as a temp cache to save us DB calls
         subscriber_cache.clear()
         self.stdout.write(self.style.SUCCESS('Starting processing'))
 
-        # Create a thread pool executor
         with ThreadPoolExecutor(max_workers=MAX_WORKERS_SEND_EMAIL) as executor:
-            # Dictionary to hold futures
             futures = {}
-            # For each content
+
             for content in contents:
-                # Try to get from cache
                 subscribers = subscriber_cache.get(content.topic)
-                # If not present in cache, find out which subscribers we need to send data to from DB
+
                 if not subscribers:
                     subscribers = list(Subscriber.objects.filter(topic=content.topic))
                     subscriber_cache[content.topic] = subscribers
 
-                # Submit each list of subscribers to the executor
                 future = executor.submit(send_newsletters, content, subscribers)
                 futures[future] = content
 
-            # Process the results as they complete
             for future in as_completed(futures):
                 content = futures[future]
                 try:
                     future.result()
-                    # Delete content from the DB after sending all emails
                     content.delete()
+                    logger.info(f"Content {content.content_text} processed and deleted")
                 except Exception as e:
-                    logger.error(f"Error processing content {content.id}: {str(e)}")
+                    logger.error(f"Error processing content {content.content_text}: {str(e)}")
 
         self.stdout.write(self.style.SUCCESS('Successfully sent newsletter emails.'))

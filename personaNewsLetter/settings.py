376980b81238407c75919e9d994cd8f0799ed0e9
Akash Tiwari <akastiwa@cisco.com>
2024-06-20 15:39:07 +0530

    committing sqllite3 file

--- personaNewsLetter/settings.py
+++ personaNewsLetter/settings.py
@@ -78,7 +78,10 @@ WSGI_APPLICATION = 'personaNewsLetter.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
